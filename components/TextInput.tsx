2bebce0ca42778a26eb379ba44bd38d44410466d
Ronak Bhatt <ronak.bhatt@crestdatasys.com>
2021-07-05 12:58:19 +0530

    password input added and code refactor

--- components/TextInput.tsx
+++ components/TextInput.tsx
@@ -1,96 +1,126 @@
-import React, { useState } from "react";
+import React from "react";
 import TextField from "@material-ui/core/TextField";
-import { WithStyles, withStyles } from "@material-ui/core/styles";
-import InputAdornment from "@material-ui/core/InputAdornment";
-import IconButton from "@material-ui/core/IconButton";
-import Visibility from "@material-ui/icons/Visibility";
-import VisibilityOff from "@material-ui/icons/VisibilityOff";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 
-interface Props extends WithStyles<typeof styles> {
+export interface InputProps {
   label?: "Error";
   error?: boolean | undefined;
   defaultValue?: string | undefined;
   disabled?: boolean | undefined;
   inputType?: string | undefined;
-  placeholder: string | undefined;
+  placeholder?: string | undefined;
   children?: React.ReactNode;
-  className?: string;
-  classes: {} | any;
-  theme?: {} | any;
+  eyeIcon?:
+    | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+    | undefined;
 }
+const inputStyleCommon: React.CSSProperties = {
+  background: "#fff",
+  width: "300px",
+  height: "50px",
+  border: "none",
+};
 
-const styles = (theme: {} | any) => ({
-  root: {
-    background: "#fff",
-    width: "300px",
-    height: "50px",
-    border: "none",
-    "&:hover": {
-      border: "none",
-      outline: "none",
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      "& > *": {
+        margin: theme.spacing(1),
+        width: "25ch",
+      },
     },
-  },
-  inputStyle: {
-    padding: "13px 46px 13px 16px",
-    boxShadow: "0 2px 20px 2px rgba(0, 0, 0, 0.2)",
-    borderRadius: "5px",
-    flexGrow: "0",
-    height: "24px",
-    "&:active": {
-      border: "solid 1px #669ae8",
+    textInput: {
+      boxShadow: "0 2px 20px 2px rgba(0, 0, 0, 0.2)",
+      borderRadius: "5px",
+      ...inputStyleCommon,
+      "&:active": {
+        border: "solid 1px #669ae8",
+      },
+      "&:hover": {
+        border: "none",
+        outline: "none",
+      },
     },
-  },
-});
-
-function Input(props: Props) {
-  const { classes, children, className, ...other } = props;
-  console.log(props);
-  const passwordInput = props.inputType === "password";
-  const [showPassword, setShowPassword] = useState(false);
-  const handleClickShowPassword = () => setShowPassword(!showPassword);
-  const handleMouseDownPassword = () => setShowPassword(!showPassword);
+    ".eye-btn": {
+      padding: "0px",
+    },
+    textInputInside: {
+      padding: "13px 46px 13px 16px",
+    }
+  })
+);
+export default function Inputs(props: InputProps) {
+  const classes = useStyles();
   return (
     <>
-      {passwordInput ? (
-        <TextField
-          variant="outlined"
-          type={showPassword ? "text" : "password"} // <-- This is where the magic happens
-          InputProps={{
-            classes: {
-              root: classes.root,
-              input: classes.inputStyle,
-            },
-            endAdornment: (
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                >
-                  {showPassword ? <Visibility /> : <VisibilityOff />}
-                </IconButton>
-              </InputAdornment>
-            ),
-          }}
-        />
-      ) : (
-        <TextField
-          variant="outlined"
-          InputProps={{
-            classes: {
-              root: classes.root,
-              input: classes.inputStyle,
-            },
-          }}
-          placeholder={props.placeholder}
-          type={props.inputType}
-          disabled={props.disabled}
-          defaultValue={props.defaultValue}
-          error={props.error}
-        />
-      )}
+      <TextField
+        variant="outlined"
+        placeholder={props.placeholder}
+        type={props.inputType}
+        disabled={props.disabled}
+        defaultValue={props.defaultValue}
+        error={props.error}
+        InputProps={{
+          endAdornment: props.eyeIcon,
+          classes: {
+            root: classes.textInput,
+            input: classes.textInputInside,
+          },
+        }}
+      />
     </>
   );
 }
 
-export default withStyles(styles)(Input);
+// const styles = (theme: {} | any) => ({
+//   root: {
+//     background: "#fff",
+//     width: "300px",
+//     height: "50px",
+//     border: "none",
+//     "&:hover": {
+//       border: "none",
+//       outline: "none",
+//     },
+//   },
+//   inputStyle: {
+//     padding: "13px 46px 13px 16px",
+//     boxShadow: "0 2px 20px 2px rgba(0, 0, 0, 0.2)",
+//     borderRadius: "5px",
+//     flexGrow: "0",
+//     height: "24px",
+//     "&:active": {
+//       border: "solid 1px #669ae8",
+//     },
+//     "&:hover": {
+//       border: "none",
+//       outline: "none",
+//     },
+//   },
+// });
+
+// function Input(props: Props) {
+//   const { classes, children, className, ...other } = props;
+
+//   return (
+//     <>
+//       <TextField
+//         variant="outlined"
+//         InputProps={{
+//           endAdornment: props.eyeIcon,
+//           classes: {
+//             root: classes.root,
+//             input: classes.inputStyle,
+//           },
+//         }}
+//         placeholder={props.placeholder}
+//         type={props.inputType}
+//         disabled={props.disabled}
+//         defaultValue={props.defaultValue}
+//         error={props.error}
+//       />
+//     </>
+//   );
+// }
+
+// export default withStyles(styles)(Input);
--- pages/inputs.tsx
+++ pages/inputs.tsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/Header";
 import Paper from "@material-ui/core/Paper";
 import TextInput from "../components/TextInput";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import IconButton from "@material-ui/core/IconButton";
+// import Visibility from "@material-ui/icons/Visibility";
+// import VisibilityOff from "@material-ui/icons/VisibilityOff";
+import Visibility from "../public/visible.png";
+import VisibilityOff from "../public/not-visible.png";
+import Avatar from "@material-ui/core/Avatar";
 
 export default function Inputs() {
+  const [showPassword, setShowPassword] = useState(false);
+  const handleClickShowPassword = () => setShowPassword(!showPassword);
+  const handleMouseDownPassword = () => setShowPassword(!showPassword);
   return (
     <div>
       <Header />
@@ -15,8 +25,38 @@ export default function Inputs() {
           <p>Password Input</p>
           <TextInput
             placeholder="Password"
-            inputType="password"
+            inputType={showPassword ? "text" : "password"}
             defaultValue="12345678"
+            eyeIcon={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                >
+                  {showPassword ? (
+                    <Avatar
+                      alt="Visibility"
+                      src={Visibility.src}
+                      style={{
+                        height: "20px",
+                        width: "20px",
+                      }}
+                    />
+                  ) : (
+                    <Avatar
+                      alt="VisibilityOff"
+                      src={VisibilityOff.src}
+                      style={{
+                        height: "20px",
+                        width: "20px",
+                      }}
+                    />
+                  )}
+                  {/* {showPassword ? <Visibility /> : <VisibilityOff />} */}
+                </IconButton>
+              </InputAdornment>
+            }
           />
           <br />
           <p>Disabled Input</p>
